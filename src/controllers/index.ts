08237275f3d064f7964f51ccec1f80b1ee9e3b27
romanmunar <roman.munar1@gmail.com>
2020-12-18 15:28:58 +0800

    added index file on controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { default as getItem } from "./item/get";
+export { default as getItemRating } from "./item/get_ratings";
+export { default as getShop } from "./shop/get";
+export { default as searchItems } from "./search_items";
